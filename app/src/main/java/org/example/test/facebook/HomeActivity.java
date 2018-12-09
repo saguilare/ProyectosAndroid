2c8c75048f41e3c734fa273d3b6a7503bd09e2cb
steven <aguilarsteven@gmail.com>
2018-12-08 22:00:13 -0600

    fix google logpout

--- app/src/main/java/org/example/test/facebook/HomeActivity.java
+++ app/src/main/java/org/example/test/facebook/HomeActivity.java
@@ -1,11 +1,17 @@
 package org.example.test.facebook;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
@@ -13,6 +19,8 @@ public class HomeActivity extends AppCompatActivity {
     // [START declare_auth]
     private FirebaseAuth mAuth;
     // [END declare_auth]
+
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +30,28 @@ public class HomeActivity extends AppCompatActivity {
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
         // [END initialize_auth]
+
+        // [START config_signin]
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // [END config_signin]
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
     public void signOut(View view) {
         if (mAuth != null){
             mAuth.signOut();
             LoginManager.getInstance().logOut();
+
+            // Google sign out
+            mGoogleSignInClient.signOut();
         }
+
+
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
